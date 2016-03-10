2522779d634528890839e460f2c9aec56627ec3e
rachnagajre <rachi.g12@gmail.com>
2016-03-09 23:44:55 -0800

    Added changes for facebook and twitter login (Twitter login does not work because it needs social settings to be done in Backendless developer console for twitter)

--- YummyRecipes/YummyRecipes/src/main/java/edu/scu/rachna/yummyrecipes/activity/LoginActivity.java
+++ YummyRecipes/YummyRecipes/src/main/java/edu/scu/rachna/yummyrecipes/activity/LoginActivity.java
@@ -17,6 +17,11 @@ import com.backendless.Backendless;
 import com.backendless.BackendlessUser;
 import com.backendless.async.callback.AsyncCallback;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import edu.scu.rachna.yummyrecipes.R;
 import edu.scu.rachna.yummyrecipes.data.Default;
 import edu.scu.rachna.yummyrecipes.data.LoadingCallback;
@@ -28,18 +33,29 @@ public class LoginActivity extends Activity
 {
     private static final int REGISTER_REQUEST_CODE = 1;
 
+    private Button loginButton;
+
+    private Button loginFacebookButton;
+
+    private Button loginTwitterButton;
+
     @Override
     protected void onCreate( Bundle savedInstanceState )
     {
-        super.onCreate( savedInstanceState );
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
         Backendless.initApp(this, Default.APPLICATION_ID, Default.ANDROID_SECRET_KEY,
                 Default.VERSION);
 
-        Button loginButton = (Button) findViewById( R.id.loginButton );
+        loginButton = (Button) findViewById( R.id.loginButton );
         loginButton.setOnClickListener( createLoginButtonListener() );
 
+        loginFacebookButton = (Button) findViewById(R.id.loginFacebookButton);
+        loginTwitterButton = (Button) findViewById(R.id.loginTwitterButton);
+
+        loginFacebookButton.setOnClickListener(createLoginFacebookButtonListener());
+        loginTwitterButton.setOnClickListener(createLoginTwitterButtonListener());
 
         makeRegistrationLink();
     }
@@ -60,10 +76,10 @@ public class LoginActivity extends Activity
         String linkText = getString( R.string.register_link );
         int linkStartIndex = registrationPrompt.toString().indexOf( linkText );
         int linkEndIndex = linkStartIndex + linkText.length();
-        registrationPrompt.setSpan( clickableSpan, linkStartIndex, linkEndIndex, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE );
+        registrationPrompt.setSpan(clickableSpan, linkStartIndex, linkEndIndex, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
 
         TextView registerPromptView = (TextView) findViewById( R.id.registerPromptText );
-        registerPromptView.setText( registrationPrompt );
+        registerPromptView.setText(registrationPrompt);
         registerPromptView.setMovementMethod(LinkMovementMethod.getInstance());
     }
 
@@ -71,15 +87,37 @@ public class LoginActivity extends Activity
     public void startRegistrationActivity()
     {
         Intent registrationIntent = new Intent( this, RegistrationActivity.class );
-        startActivityForResult( registrationIntent, REGISTER_REQUEST_CODE );
+        startActivityForResult(registrationIntent, REGISTER_REQUEST_CODE);
     }
 
 
     public void loginUser( String email, String password, AsyncCallback<BackendlessUser> loginCallback )
     {
-        Backendless.UserService.login( email, password, loginCallback,true);
+        Backendless.UserService.login(email, password, loginCallback, true);
     }
 
+    public void loginUserWithFacebook(LoadingCallback<BackendlessUser> loginCallback) {
+        Map<String, String> facebookFieldMappings = new HashMap<>();
+        facebookFieldMappings.put( "email", "email" );
+        facebookFieldMappings.put( "name", "name" );
+
+        List<String> permissions = new ArrayList<String>();
+        permissions.add("email");
+        permissions.add("public_profile");
+
+        Backendless.UserService.loginWithFacebook(LoginActivity.this, null, facebookFieldMappings, permissions, loginCallback, true);
+    }
+
+    public void loginUserWithTwitter(LoadingCallback<BackendlessUser> loginCallback) {
+        loginCallback.hideLoading();
+
+        Map<String, String> twitterFieldsMappings = new HashMap<String, String>();
+        twitterFieldsMappings.put("name", "name");
+        //twitterFieldsMappings.put("email", "screen_name");
+
+        //Toast.makeText(LoginActivity.this, "Work in progress!!", Toast.LENGTH_LONG).show();
+        Backendless.UserService.loginWithTwitter(LoginActivity.this, null, twitterFieldsMappings, loginCallback, true);
+    }
 
     public View.OnClickListener createLoginButtonListener()
     {
@@ -97,10 +135,33 @@ public class LoginActivity extends Activity
                 if( isLoginValuesValid( email, password ) )
                 {
                     LoadingCallback<BackendlessUser> loginCallback = createLoginCallback();
-
                     loginCallback.showLoading();
                     loginUser( email.toString(), password.toString(), loginCallback );
                 }
+            }
+        };
+    }
+
+    public View.OnClickListener createLoginFacebookButtonListener() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LoadingCallback<BackendlessUser> loginCallback = createLoginCallback();
+                loginCallback.showLoading();
+
+                loginUserWithFacebook(loginCallback);
+            }
+        };
+    }
+
+    public View.OnClickListener createLoginTwitterButtonListener() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LoadingCallback<BackendlessUser> loginCallback = createLoginCallback();
+                loginCallback.showLoading();
+
+                loginUserWithTwitter(loginCallback);
             }
         };
     }
@@ -121,7 +182,6 @@ public class LoginActivity extends Activity
                 super.handleResponse(loggedInUser);
                 //Toast.makeText(getApplicationContext(),"Works", Toast.LENGTH_LONG).show();
                 startDashboardActivity(loggedInUser);
-
             }
         };
     }
